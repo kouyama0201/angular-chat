ee90b996eae558a44f3ead519b3cb3720ac80a6d
kouyama0201 <kami19900201@gmail.com>
2023-03-19 17:14:15 +0900

    89. ヘッダーを作成する

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'ac-root',
   template: `
+    <ac-header></ac-header>
     <ac-chat></ac-chat>
   `,
   styleUrls: ['./app.component.css']
