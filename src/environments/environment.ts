487a815a5c046e6e4daf9c0efa60cca96827d8d9
kouyama0201 <kami19900201@gmail.com>
2023-03-19 15:26:41 +0900

    80. FirebaseとAngularを連携する

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyD86faL9NzvVbhL4mmcHbFWmyyuLvULoRw",
+    authDomain: "angular-chat-e7b0b.firebaseapp.com",
+    projectId: "angular-chat-e7b0b",
+    storageBucket: "angular-chat-e7b0b.appspot.com",
+    messagingSenderId: "390453137008",
+    appId: "1:390453137008:web:2e47471548c4c196f5eeb5"
+  }
 };
 
 /*
