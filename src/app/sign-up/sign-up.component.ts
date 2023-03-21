9d5cfeccf20dd640ec72a2f4242cf6902d790ff0
kouyama0201 <kami19900201@gmail.com>
2023-03-22 00:15:33 +0900

    96. usersモジュールを作成する

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -22,6 +22,6 @@ export class SignUpComponent implements OnInit {
     const { email, password } = form.value;
 
     this.authService.create(email, password)
-      .then(() => this.router.navigateByUrl('/'));
+      .then(() => this.router.navigateByUrl('/users/new'));
   }
 }
