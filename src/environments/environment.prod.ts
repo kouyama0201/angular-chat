57150663ed6b343ca2426bc480640c92324bc6bb
kouyama0201 <kami19900201@gmail.com>
2023-04-02 00:22:27 +0900

    104. アプリケーションを公開する

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD86faL9NzvVbhL4mmcHbFWmyyuLvULoRw",
+    authDomain: "angular-chat-e7b0b.firebaseapp.com",
+    projectId: "angular-chat-e7b0b",
+    storageBucket: "angular-chat-e7b0b.appspot.com",
+    messagingSenderId: "390453137008",
+    appId: "1:390453137008:web:2e47471548c4c196f5eeb5"
+  }
 };
